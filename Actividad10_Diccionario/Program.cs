a985547f0dcea3e8f0c8e8615c6efba389f4ac05
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-04 16:36:46 -0600

    Agregar archivos de proyecto.

---
+++ Actividad10_Diccionario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Actividad10_Diccionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
