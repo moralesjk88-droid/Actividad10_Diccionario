5541985c284e7ce42c5dadbbab9a437cad445e46
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-04 16:55:33 -0600

    Primer programa hecho

--- Actividad10_Diccionario/Program.cs
+++ Actividad10_Diccionario/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary <int ,string  > estudiantes = new Dictionary<int,string>();
+            estudiantes.Add(202618, "Maria");
+            estudiantes.Add(202620, "Juan");
+            estudiantes.Add(202619, "Carlos");
+
+            foreach (var item in estudiantes)
+            {
+                Console.WriteLine(item.Key + "-"+item.Value);   
+            }
+            Console.WriteLine("Nombre del estudiante en especifico:_" + estudiantes[202620]);
+
+
+
         }
+
     }
 }
