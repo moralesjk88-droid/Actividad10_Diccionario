54234a86e32f56db25d3d496464ce829bb5bdeb6
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-04 17:41:24 -0600

    prueva 2, para rama1

--- Actividad10_Diccionario/Program.cs
+++ Actividad10_Diccionario/Program.cs
@@ -4,16 +4,7 @@
     {
         static void Main(string[] args)
         {
-            /*  Dictionary <int ,string  > estudiantes = new Dictionary<int,string>();
-              estudiantes.Add(202618, "Maria");
-              estudiantes.Add(202620, "Juan");
-              estudiantes.Add(202619, "Carlos");
-
-              foreach (var item in estudiantes)
-              {
-                  Console.WriteLine(item.Key + "-"+item.Value);   
-              }
-              Console.WriteLine("Nombre del estudiante en especifico:_" + estudiantes[202620]);*/
+            
 
             //ejercicios2
             Dictionary<int, string> productos = new Dictionary<int, string>();
